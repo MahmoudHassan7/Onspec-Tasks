baa1d6df34ca726c36e1724fb32fc236b50d1e5f
abdallh <obad.mohamed20@gmail.com>
2023-05-03 22:00:53 +0300

    upload project

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace WpfApp2
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            DBSinglton.creatDbIfNotExists();
+            DBSinglton._ConnectionString = "Data Source=MyDatabase.sqlite;Version=3;";
+
+            DBSinglton._Query = "CREATE TABLE IF NOT EXISTS customers (id INT PRIMARY KEY , name TEXT, email TEXT);CREATE TABLE IF NOT EXISTS orders (id INT PRIMARY KEY , customer_id INT, total REAL, order_date TEXT,  FOREIGN KEY(customer_id) REFERENCES customers(id));";
+            SQLiteConnection c = DBSinglton.CreateConnection();
+            DBSinglton.ExecuteQuery(c);
+
+            DBSinglton._Query = $"INSERT OR IGNORE INTO 'customers' (id,name,email) VALUES";
+            for (int i = 0; i < 5; i++)
+            {
+                DBSinglton._Query += $"('{i}','N{i}','E{i}'),";
+
+            }
+            DBSinglton._Query = DBSinglton._Query.Remove(DBSinglton._Query.Length - 1, 1);
+            DBSinglton._Query += ";";
+            DBSinglton.ExecuteQuery(c);
+
+            DBSinglton._Query = "SELECT  * FROM customers";
+            DBSinglton.ExecuteQuery(c);
+
+            SQLiteDataAdapter sQLiteDataAdapter = new SQLiteDataAdapter(DBSinglton._sqliteCmd);
+            DataTable dataTable = new DataTable("customers");
+
+            sQLiteDataAdapter.Fill(dataTable);
+            CustomerGrid.ItemsSource = dataTable.DefaultView;
+            sQLiteDataAdapter.Update(dataTable);
+
+            DBSinglton.closeConnection();
+            c.Close();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            w.Show();
+            //this.Close();
+        }
+
+    }
+}
---
+++ WpfApp2/Window2.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // "AS" keyword dose not work with me XD
+            SQLiteConnection c = DBSinglton.CreateConnection();
+            DBSinglton._Query = "SELECT name,  email FROM customers INNER JOIN (SELECT * FROM (SELECT * FROM orders WHERE total > 1000 GROUP By 2 HAVING count(*) >= 2)) ON customer_id = customers.id;";
+            DBSinglton.ExecuteQuery(c);
+
+            SQLiteDataAdapter sQLiteDataAdapter = new SQLiteDataAdapter(DBSinglton._sqliteCmd);
+            DataTable dataTable = new DataTable();
+
+            sQLiteDataAdapter.Fill(dataTable);
+            qGrid.ItemsSource = dataTable.DefaultView;
+            sQLiteDataAdapter.Update(dataTable);
+
+            DBSinglton.closeConnection();
+            c.Close();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            test.Content = "Write a SQL query to retrieve the names and email addresses of customers \r\n" +
+                "who haveplaced at least two orders with a total value greater than $1000.\r\n" +
+                "SELECT name,  email FROM customers \r\n " +
+                "INNER JOIN \r\n" +
+                "(SELECT * FROM (SELECT * FROM orders WHERE total > 1000 GROUP By 2 HAVING count(*) >= 2)) \r\n" +
+                "ON customer_id = customers.id;";
+        }
+    }
+}
