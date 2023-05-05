e9a1cc51c445f33135a8e736208b2c40ab983f08
abdallh <obad.mohamed20@gmail.com>
2023-05-05 03:36:11 +0300

    Project upload

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Data_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!data.SelectedDate.HasValue)
+            {
+                LabelCont.Content = "Select dates";
+                return;
+            }
+            DateTime start = data.SelectedDate.Value.Date;
+            DateTime finish = DateTime.Now;
+            TimeSpan Span = finish - start;
+            
+            try
+            {
+                DateTime Age = DateTime.MinValue + Span;
+                int Years = Age.Year - 1;
+                int Months = Age.Month - 1;
+                int Days = Age.Day - 1;
+                LabelCont.Content = $"Your current age is \r\n {Years} years, {Months} months and {Days} days";
+            }
+            catch
+            {
+                LabelCont.Content = "Date error";
+            }
+            
+        }
+    }
+}
